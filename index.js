7251ed3c855ab7c7919d5323b24beabc65a08421
jpeterson0514 <>
2021-05-20 20:33:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//What should each of the following expressions return? Once you have an answer, click the Run button to see if you're right
+
+// Practice with AND
+0 && false;
+
+0 && true;
+
+true && NaN;
+
+true && !1;
+
+!0 && "This is a string";
+
+!0 && "";
+
+!0 && !!"";
+
+Practice with OR
+
+0 || false;
+
+true || false;
+
+true || 1;
+
+!true || !false
+
+!1 || !0
